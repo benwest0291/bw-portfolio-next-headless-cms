6c47dce0042f9cf4660239a113a32a6b52bc7ffa
Ben West <benwest0291@outlook.com>
2023-03-07 19:48:44 +0000

    tweaked SVG and image size

--- components/About/About.js
+++ components/About/About.js
@@ -9,12 +9,12 @@ const About = ({ setting }) =>{
           <div className="row">
              <div className="col-12 col-md-6">
                 <div className="d-flex justify-content-center">
-                 <Image src="/about.svg" alt="banner image" width={400} height={400} className="about__image mt-5"/>
+              <Image src="/about.svg" alt="banner image" width={385} height={385} className="about__image mt-5"/>
                 </div>
               </div>
               <div className="col-12 col-md-6">
                   <div className="about__content mt-4">
-                      <h2>{aboutHeading}</h2>
+                      <h2>{ aboutHeading }</h2>
                       <div className="about__line mb-4"></div>
                       <p className="mt-4 mb-4">{ aboutParagraph }</p>
                       <ul className=" about__skills d-flex mb-5 flex-wrap">
