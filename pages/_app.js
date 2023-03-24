7806436e060b33fbfa245dba3f75d37e79e423a9
Ben West <benwest0291@outlook.com>
2023-03-24 20:53:39 +0000

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,32 @@
 import '../scss/Globals.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import Layout from '../components/Layout'
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-RKW5WFM8M1" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RKW5WFM8M1', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
