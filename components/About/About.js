c20e87d7e8f88925f296ec44092f741fa4ab35d3
Ben West <benwest0291@outlook.com>
2023-03-24 12:42:16 +0000

    added aos lib and scc animations

--- components/About/About.js
+++ components/About/About.js
@@ -1,33 +1,33 @@
 import Image from "next/image"
 
-const About = ({ setting }) =>{
+const About = ({ setting }) => {
 
   const { aboutHeading, aboutParagraph, skills } = setting.fields
   return (
     <section className="about" id="about">
       <div className="container">
-          <div className="row">
-             <div className="col-12 col-md-6">
-                <div className="d-flex justify-content-center">
-              <Image src="/about.svg" alt="banner image" width={385} height={385} className="about__image mt-5"/>
-                </div>
-              </div>
-              <div className="col-12 col-md-6">
-                  <div className="about__content mt-4">
-                      <h2>{ aboutHeading }</h2>
-                      <div className="about__line mb-4"></div>
-                      <p className="mt-4 mb-4">{ aboutParagraph }</p>
-                      <ul className=" about__skills d-flex mb-5 flex-wrap">
-                          {skills.map(skill => (
-                            <li key={skill}>{skill}</li>
-                          ))}
-                      </ul>
-                      </div>
-                  </div>
-              </div>
+        <div className="row">
+          <div className="col-12 col-md-6" data-aos="fade-right">
+            <div className="d-flex justify-content-center">
+              <Image src="/about.svg" alt="banner image" width={385} height={385} className="about__image mt-5" />
+            </div>
           </div>
-      </section>
-   )
+          <div className="col-12 col-md-6" data-aos="fade-left">
+            <div className="about__content mt-4">
+              <h2>{aboutHeading}</h2>
+              <div className="about__line mb-4"></div>
+              <p className="mt-4 mb-4">{aboutParagraph}</p>
+              <ul className=" about__skills d-flex mb-5 flex-wrap">
+                {skills.map(skill => (
+                  <li key={skill}>{skill}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About
--- components/Projects/FeaturedProject.js
+++ components/Projects/FeaturedProject.js
@@ -6,11 +6,13 @@ const FeaturedProject = ({ setting }) => {
   return (
     <section className="project pb-5">
       <div className="container">
-        <h2 className="mt-5">{projects}</h2>
-        <div className="project__line mb-5"></div>
-        <div className="green__line mb-4"></div>
-        <Image className="project__image" src="/project.png" width={700} height={400} alt="project image" />
-        <div className="project__space">
+        <div data-aos="fade-right">
+          <h2 className="mt-5" >{projects}</h2>
+          <div className="project__line mb-5"></div>
+          <div className="green__line mb-4"></div>
+          <Image className="project__image" src="/project.png" width={700} height={400} alt="project image" />
+        </div>
+        <div className="project__space" data-aos="fade-left" >
           <ProjectCard
             title={projectTitle}
             info={projectInfo}
